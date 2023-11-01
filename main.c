5ea1b3d65d7ccaac9c338395fa3ad8f1a27c3ce7
EnriqueWood <ewoodg@hotmail.com>
2023-10-31 23:09:07 -0300

    Fix typo in BinariesLauncher folder

--- main.c
+++ main.c
@@ -3,8 +3,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
-#include "definitions.h"
-#include "BinariesLancher/launcher.h"
+#include "BinariesLauncher/launcher.h"
 #include "Configuration/configurationManager.h"
 #include "debug.h"
 
